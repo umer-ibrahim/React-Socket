d429dd09b462ec9c8f910cd310d3cfa6bebdad22
Muhammad Usman <muhammad@eze2travel.com>
2022-08-03 20:59:36 +0500

    Push Notification Added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Space, Input, Button} from 'antd';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { getMessaging, getToken } from "firebase/messaging";
+import FirebaseApp from './Firebase'
+import {  } from "firebase/messaging";
+const Socket = io.connect("http://localhost:8080")
+
 
 function App() {
+
+  const [msg, setMsg] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  const sendMsg = ()=>{
+    Socket.emit("hi",msg)
+  }
+  
+  const generateToken = ()=>{
+    // Initialize Firebase Cloud Messaging and get a reference to the service
+    const messaging = getMessaging(FirebaseApp);
+    getToken(messaging, {vapidKey: "BHjpZ3PljaIZ_BjIYlm443ylUGUWqjpsHrvfRE1B1U3J7sEaXMik7KM-cRn4WR6pqx58S5isX7XEvtgyb6jXQeo"})
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log(currentToken);
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+      }
+    }).catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+      // ...
+    });
+  }
+
+
+  useEffect(()=>{
+    Socket.on("hi",
+    (msg)=>{
+      setMsgs([...msgs, msg]);
+      setMsg("");
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Space direction='vertical'>
+          <h5>{msg}</h5>
+          <Input placeholder="Message" onChange={(e)=>setMsg(e.target.value)}/>
+          <Button type="primary" onClick={sendMsg} >Primary Button</Button>
+          <Button type="primary" onClick={generateToken} >Generate Button</Button>
+          {
+            msgs.map(item =><h6>{item}</h6>)
+          }
+        </Space>
       </header>
     </div>
   );
---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCTuKQkNtZiYp67Jl5qduLjknJWyHV3zjk",
+    authDomain: "redfire-65891.firebaseapp.com",
+    projectId: "redfire-65891",
+    storageBucket: "redfire-65891.appspot.com",
+    messagingSenderId: "411794587840",
+    appId: "1:411794587840:web:4cfd672e148398415eed25",
+    measurementId: "G-SHKS7C2V1J"
+  };
+  const Firebase = initializeApp(firebaseConfig);
+export default Firebase;
+
